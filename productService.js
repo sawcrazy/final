0c8a2703933bbefa6ae39f8e80e3e1674b6451ac
Your Name <you@example.com>
2022-08-17 19:32:59 +0300

    final

---
+++ productService.js
@@ -0,0 +1,21 @@
+class ProductService {
+    constructor(products = []) {
+        this.products = products
+    }
+
+    get(index){
+        return this.products[index]
+    }
+    filterBy(search = '') {
+        if(!search.trim()) return this.products
+
+        return this.products.filter(product => {
+            return product.title.toLowerCase().includes(search.toLowerCase())
+        })
+    }
+    getById(id) {
+        return this.products.find(product => {
+            return product.id === id
+        })
+    }
+}
